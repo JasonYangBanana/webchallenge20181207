a256d99f8efd4e37f69e8fdc4e6c94103e51272a
JasonYang <jason19901207@gmail.com>
2018-12-14 03:48:25 +0800

    瀑布流完成

---
+++ js/kerway.js
@@ -0,0 +1,6 @@
+// iso.arrange({
+//     filter: function (itemElem) {
+//         var number = itemElem.querySelector('.number').innerText;
+//         return parseInt(number, 10) > 50;
+//     }
+// });
