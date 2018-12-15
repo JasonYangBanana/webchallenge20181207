cf921e81ad0b8ff2c453cf4daa9d291d81ee2a98
JasonYang <jason19901207@gmail.com>
2018-12-15 17:30:53 +0800

    完成switch圖示與arrowUp圖示的filter與動畫

--- js/kerway.js
+++ js/kerway.js
@@ -45,6 +45,16 @@ $(document).ready(function () {
         }, 1000);
     });
     $('.nineDots').click(function () {
+        $grid.isotope({
+            filter: '*'
+        });
+        $('.kerwaySubtitle').text('Works');
+        $('.kerwaySubtitle').append('<br><span class="kerwayChineseSubtitle">作品</span>');
+        $('html, body').animate({
+            scrollTop: $("#kerwayWorks").offset().top
+        }, 1000);
+    });
+    $('.arrowUp').click(function () {
         $('html, body').animate({
             scrollTop: $("#kerwayWorks").offset().top
         }, 1000);
