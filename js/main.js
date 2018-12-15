bc170c0a762231663e7f00cdacf94884351d2c0d
Kai <b10223052@yuntech.org.tw>
2018-12-15 19:06:55 +0800

    fix some js

--- js/main.js
+++ js/main.js
@@ -36,21 +36,22 @@ function animationMenu() {
   }
   menuStatus = !menuStatus
 }
-let kerwayBanner = document.querySelector(".kerwayBanner")
-let kerwayBackground = document.querySelector(".kerwayBackground")
-kerwayBackground.style.transform = " matrix(1.05, 0, 0, 1.05, 0, 0)"
-kerwayBanner.addEventListener("mousemove", function(e) {
-  let moveRect = 10
-  let x = e.offsetX
-  let y = e.offsetY
-  if (this !== e.target) {
-    x += e.target.offsetLeft;
-    y += e.target.offsetTop;
-  }
-  let width = kerwayBackground.offsetWidth
-  let height = kerwayBackground.offsetHeight
-  let xMove = Math.round((x / width - 0.5) * moveRect);
-  let yMove = Math.round((y / height - 0.5) * moveRect);
-  let shadowLevel = (Math.abs(xMove) + Math.abs(yMove)) / 2 / moveRect * 3
-  kerwayBackground.style.transform = `matrix(1.03, 0, 0, 1.03, ${xMove}, ${yMove})`
-})
+
+// let kerwayBanner = document.querySelector(".kerwayBanner")
+// let kerwayBackground = document.querySelector(".kerwayBackground")
+// kerwayBackground.style.transform = " matrix(1.05, 0, 0, 1.05, 0, 0)"
+// kerwayBanner.addEventListener("mousemove", function(e) {
+//   let moveRect = 10
+//   let x = e.offsetX
+//   let y = e.offsetY
+//   if (this !== e.target) {
+//     x += e.target.offsetLeft;
+//     y += e.target.offsetTop;
+//   }
+//   let width = kerwayBackground.offsetWidth
+//   let height = kerwayBackground.offsetHeight
+//   let xMove = Math.round((x / width - 0.5) * moveRect);
+//   let yMove = Math.round((y / height - 0.5) * moveRect);
+//   let shadowLevel = (Math.abs(xMove) + Math.abs(yMove)) / 2 / moveRect * 3
+//   kerwayBackground.style.transform = `matrix(1.03, 0, 0, 1.03, ${xMove}, ${yMove})`
+// })
