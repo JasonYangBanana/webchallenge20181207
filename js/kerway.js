4567b30d8741978279ce7f07ed216a1fa4c6a6de
JasonYang <jason19901207@gmail.com>
2018-12-15 17:16:06 +0800

    新增filter點擊後的移動效果

--- js/kerway.js
+++ js/kerway.js
@@ -11,20 +11,45 @@ $(document).ready(function () {
         $grid.isotope({
             filter: '.melody'
         });
+        $('.kerwaySubtitle').text('Melody Lin');
+        $('html, body').animate({
+            scrollTop: $("#kerwayWorks").offset().top
+        }, 1000);
+
     });
     $('li[data-filter=mina]').click(function () {
         $grid.isotope({
             filter: '.mina'
         });
+        $('.kerwaySubtitle').text('Mina');
+        $('html, body').animate({
+            scrollTop: $("#kerwayWorks").offset().top
+        }, 1000);
     });
     $('li[data-filter=momo]').click(function () {
         $grid.isotope({
             filter: '.momo'
         });
+        $('.kerwaySubtitle').text('Momo');
+        $('html, body').animate({
+            scrollTop: $("#kerwayWorks").offset().top
+        }, 1000);
     });
     $('li[data-filter=miu]').click(function () {
         $grid.isotope({
             filter: '.miu'
         });
+        $('.kerwaySubtitle').text('Miu');
+        $('html, body').animate({
+            scrollTop: $("#kerwayWorks").offset().top
+        }, 1000);
+    });
+    $('.nineDots').click(function () {
+        $grid.isotope({
+            filter: '*'
+        });
+        $('html, body').animate({
+            scrollTop: $("#kerwayWorks").offset().top
+        }, 1000);
     });
 });
